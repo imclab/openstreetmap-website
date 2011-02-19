cc95a2b569301fc68828d214a8fb95a962213ad1
jywarren <jeff@unterbahn.com>
2011-02-19 00:45:31 -0500

    added davetroy-geohash ilne

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,8 @@
 ENV['RAILS_ENV'] ||= 'development'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
+#RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Set the server URL
 SERVER_URL = ENV['OSM_SERVER_URL'] || 'planet.cartagen.org'
